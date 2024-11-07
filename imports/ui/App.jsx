6623c77c34d407d05d901ed07322b842705ca293
Lin Latt <lattln.tech@gmail.com>
2024-11-07 16:29:38 -0500

    update 2
    + added chartjs
    + added react-chartjs-2
    + added example chart

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { Hello } from './Hello.jsx';
 import { Info } from './Info.jsx';
 import { NavBar } from './NavBar.jsx';
+import { ExampleChart } from './ExampleChart.jsx';
 
 export const App = () => (
   <div>
     <NavBar/>
     <Hello/>
     <Info/>
+    <ExampleChart/>
     
   </div>
 );
