4484811a257d31d9854233f831e49f3e8efc771c
Lin Latt <lattln.tech@gmail.com>
2024-11-21 17:04:38 -0500

    restructure

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
-import { Hello } from './Hello.jsx';
-import { Info } from './Info.jsx';
-import { NavBar } from './NavBar.jsx';
-import { ExampleChart } from './ExampleChart.jsx';
+import { Hello } from './components/Hello.jsx';
+import { Info } from './components/Info.jsx';
+import { NavBar } from './components/NavBar.jsx';
+import { ExampleChart } from './components/ExampleChart.jsx';
 
 export const App = () => (
-  <div>
-    <NavBar/>
-    <Hello/>
-    <Info/>
-    <ExampleChart/>
-    
-  </div>
+    <div>
+        <NavBar />
+        <Hello />
+        <Info />
+        <ExampleChart />
+
+    </div>
 );
