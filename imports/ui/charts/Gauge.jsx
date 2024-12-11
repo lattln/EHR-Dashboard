4711bc9e5abddf87f58deb17077b1d7ca3d7358e
jengledow <jmengledow5@gmail.com>
2024-12-11 07:07:34 -0500

    fixed weird state management issue

--- imports/ui/charts/Gauge.jsx
+++ imports/ui/charts/Gauge.jsx
@@ -2,29 +2,34 @@ import GaugeComponent from "react-gauge-component";
 import React, { useEffect, useState } from "react";
 
 function Gauge({ max, min, steps, loinc, className, label }){
-    const [value, setVal] = useState(5);
+    const [value, setVal] = useState(null);
 
     useEffect(() => {
-        Meteor.callAsync(`patient.${loinc}`, 1).then((data) => {
-            let value = data[data.length - 1].valueQuantity;
-            setVal(value.value);
-        })
+        async function getBMI(){
+            let res = await Meteor.callAsync(`patient.${loinc}`, 1);
+            setVal(res[res.length - 1].valueQuantity.value);
+        }
+
+        getBMI();
     }, []);
 
     return (
         <div className={className}>
             <h3>{label}</h3>
-            <GaugeComponent
-                minValue={min}
-                maxValue={max}
-                value={value}
-                gradient={true}
-                type='radial'
-                arc={{
-                    nbSubArcs: steps.length,
-                    subArcs: steps 
-                }}
-            />
+            {value ?
+                <GaugeComponent
+                    minValue={min}
+                    maxValue={max}
+                    value={value}
+                    gradient={true}
+                    type='radial'
+                    arc={{
+                        nbSubArcs: steps.length,
+                        subArcs: steps 
+                    }}
+                />
+                : 'BMI data not found'
+            }
         </div>
     )
 }
