5c7bc647b0d37ba9889d61f2164633d4bcd92891
Austin Decker <deckaj01@pfw.edu>
2025-01-27 16:10:09 -0500

    fixed imports

--- imports/api/Fhir/fhirMethods.js
+++ imports/api/Fhir/fhirMethods.js
@@ -3,9 +3,9 @@
  * these methods will take the results from the query to the server and transform it for the front end to utilize 
  * when creating graphs off of patient medical data.
  */
-import { fhirClient } from './fhirclient.mjs';
+import { fhirClient } from './fhirclient.js';
 import { Meteor } from 'meteor/meteor';
-import { LOINC_MAPPING } from '../Loinc/loincConstants.mjs';
+import { LOINC_MAPPING } from '../Loinc/loincConstants.js';
 
 // takes only the useful information about the given observation
 function transformObservationInformation(observationResource) 
