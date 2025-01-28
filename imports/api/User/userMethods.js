4d9b316ae5039848f72e9803e851e0d2a9f44702
AustinDecker <deckaj01@pfw.edu>
2025-01-28 18:22:25 -0500

    oh boy oh boy, i sure hope this doesn't break anything. TEE HEE.

--- imports/api/User/userMethods.js
+++ imports/api/User/userMethods.js
@@ -1,17 +1,63 @@
 import { Meteor } from 'meteor/meteor'; 
 import { Roles } from 'meteor/alanning:roles';
 
-Meteor.methods({
-    async 'user.signup'(email, password, role) {
-        const userId = await Accounts.createUser({ email, password });
+async function patientHandler(options = {firstName, lastName, email, password, phoneNumber, dob, role}) {
+    if (!firstName || !lastName || !phoneNumber || !dob) {
+        throw new Meteor.Error("Not Enough Information", "the provided user information is not enough to create a patient account");
+    }
 
-        if (Meteor.isServer) {
-            Roles.addUsersToRolesAsync(userId, [role]);
-            console.log(`${userId} given role: ${role}.`);
+    let userID;
+    try {
+        userID = await Accounts.createUserAsync({email, password, firstName, lastName, phoneNumber, dob, role});
+    }
+    catch (error){
+        if (error.error === 403 && error.reason === 'Email already exists.') {
+            throw new Meteor.Error('Email Already Exists', 'The email address is already in use. Please use a different email.');
+        }
+        else throw error;
+
+    }
+
+    return userID 
+} 
+
+function clinicianHandler(options) {
+    //TODO
+}
+
+function adminHandler(options) {
+    //TODO
+}
+
+Meteor.methods({
+    async 'user.signup'(userInformation) {
+
+        const {email, password, role} = userInformation;
+        
+        if(!email || !password || !role){
+            throw new Meteor.Error("Not Enough Information", "the provided user information is not enough for account creation.")
+        }
+        //let userId = Accounts.createUser({ email, password, firstName, lastName, dob, phoneNumber});
+        let userID;
+
+        switch (role) {
+            case "patient":
+                userID = patientHandler(userInformation)
+                break;
+            case "clinician":
+                userID = clinicianHandler(userInformation);
+                throw new Meteor.Error("Unimplemented", "Current feature is not implemented yet.");
+                break;
+            case "admin":
+                throw new Meteor.Error("Unimplemented", "Current feature is not implemented yet.");
+            default:
+                throw new Meteor.Error("Invalid Role Assignment", "The role provided is invalid.")
         }
 
-        console.log(`User created with id: ${userId} and role ${role}.`);   // this should be logged later on
-        return userId;
+        await Roles.addUsersToRolesAsync(userID, [role]);
+        console.log(`${userID} given role: ${role}.`);
+        console.log(`User created with id: ${userID} and role ${role}.`);   // this should be logged later on
+        return userID;
     },
     /* example method for authorizing certain users to perform tasks
     *  
--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -1,33 +1,45 @@
 import { Accounts } from 'meteor/accounts-base';
-import { Mongo } from 'meteor/mongo';
 import { Roles } from 'meteor/alanning:roles';
 import { roles } from '../api/User/userRoles.js';
 
+
 //WIP account creation hook. logic for adding roles, checking if an account already exists in the fhir server and doesnt already exist in the accounts db.
 Accounts.onCreateUser(async (options, user) => {
-
-  let {role, firstName, lastName, dob, phoneNumber} = options;
-  const validRoles = roles.includes(role);
-
-  if (!role || !validRoles){
-    throw new Meteor.Error("Invalid Role");
+  // Add custom fields to the user object
+  if (options.profile) {
+    user.profile = options.profile;
   }
 
-  if(!firstName || !lastName, !dob, !phoneNumber) {
-    throw new Meteor.Error("missing required user information.");
+  switch (options.role) {
+    case "patient":
+      const {firstName, lastName, dob, phoneNumber} = options;
+      const fhirID = await Meteor.callAsync("patient.findByInfo", {
+        patientGivenName: firstName,
+        patientFamilyName: lastName,
+        patientDOB: dob,
+        patientPhoneNumber: phoneNumber,
+      });
+
+      if (fhirID !== -1) {
+        user.fhirID = fhirID;
+      } 
+      else {
+        throw new Meteor.Error("Patient Not Found", "A patient with the provided information does not exist in the fhir server.");
+      }
+
+      user.profile.firstName = firstName;
+      user.profile.lastName = lastName;
+      user.profile.dob = dob;
+      user.profile.phoneNumber = phoneNumber;
+
+      break;
+    case "clinician":
+      break;
+    case "admin":
+      break
+    default:
+      throw new Meteor.Error("Invalid Role Assignment", "The role provided is invalid.")
   }
-  
-  let fhirSearchPromise = Meteor.callAsync("patient.findByInfo", {
-    patientGivenName: firstName, 
-    patientFamilyName: lastName, 
-    patientDOB: dob,
-    patientPhoneNumber: phoneNumber 
-  });
-
-  //let databaseSearchPromise = 
-
-  
-  
 
   return user;
 });
