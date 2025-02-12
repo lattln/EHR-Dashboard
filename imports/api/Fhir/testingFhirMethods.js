549eabf477984cf3d8991d22a874cf5d90b7efe5
AustinDecker <deckaj01@pfw.edu>
2025-02-12 16:11:38 -0500

    removed unnessary files.

--- imports/api/Fhir/testingFhirMethods.js
+++
@@ -1,8 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-
-async function main(){
-    const result = await Meteor.callAsync("patient.getRecentLabs", 1, 100);
-    let jsonString = JSON.stringify(result, null, 4);
-    console.log(jsonString);
-}
-main();
