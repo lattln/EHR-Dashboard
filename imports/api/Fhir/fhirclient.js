8732d0a0ff1d632eb489745585616accd810153e
AustinDecker <deckaj01@pfw.edu>
2025-02-13 13:25:35 -0500

    fixed minor bug where js was not recognizing the fhirClient being imported.

--- imports/api/Fhir/fhirclient.js
+++ imports/api/Fhir/fhirclient.js
@@ -5,10 +5,6 @@
  * provides a variety of methods to interact with the server, provided the 
  * server supports the actions.
  */
-const fhirClient = new Client({
+export const fhirClient = new Client({
     baseUrl: `http://localhost:8080/fhir`
 })
-
-export default {
-    fhirClient,
-}
