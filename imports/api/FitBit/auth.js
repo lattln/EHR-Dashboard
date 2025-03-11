c9fa7ecb2a7586d1cf2c024d77bbe3c4306b31f3
jengledow <jmengledow5@gmail.com>
2025-03-11 17:14:22 -0400

    added jose to encrypt/decrypt jwt's (fitbit auth tokens) -- allowing for constant sign in

--- imports/api/FitBit/auth.js
+++ imports/api/FitBit/auth.js
@@ -1,10 +1,13 @@
+import { EncryptJWT, jwtDecrypt } from "jose";
 /*
     CONSTANTS:
 
-        token duration: amount of time a token is valid for in milliseconds 
+        token duration: amount of time a token is valid for in milliseconds
+        jwt secret: until we figure out env
 */
 
-const TOKEN_DURATION = 28800;
+const TOKEN_DURATION = 1;
+const JWT_SECRET = new Uint8Array([225, 141, 211, 39, 40, 184, 115, 65, 186, 157, 142, 105, 92, 110, 156, 143, 105, 85, 145, 239, 172, 35, 179, 190, 78, 223, 53, 224, 219, 249, 33, 143]);
 
 function sha256(plain){
     const encoder = new TextEncoder();
@@ -81,31 +84,41 @@ async function getToken(code){
             errors: res.errors
         }
     } else {
+
         res.issued_at = Date.now();
-        localStorage.setItem('fitbit-token', JSON.stringify(res));
+        const jwt = await encJWT(res);
+        localStorage.setItem('fitbit-token', jwt);
+
         return {
             success: true
         }
     }
 }
 
-function isValidToken(){
-    let token = localStorage.getItem('fitbit-token');
-    if(Date.now() - TOKEN_DURATION < JSON.parse(token).issued_at){
-        return true;
-    }
-
-    return false;
+async function decJWT(JWE){
+    const { payload } = await jwtDecrypt(JWE, JWT_SECRET);
+    return payload;
 }
 
-async function refreshToken(){
-    /*if(!localStorage.getItem('fitbit-token')){
-        return {
-            success: false,
-            msg: 'No token available'
-        }
-    }*/
+async function encJWT(token){
+    const jwt = await new EncryptJWT(token).setProtectedHeader({ alg: 'dir', enc: 'A128CBC-HS256' }).setIssuedAt().encrypt(JWT_SECRET);
+    console.log(jwt);
 
+    return jwt;
+}
+
+//Checks if the token has expired and needs to be renewed
+function isValidToken(jwe){
+    let jwt = decJWT(jwe);
+    let diff = Date.now() - TOKEN_DURATION;
+    if(diff > jwt.issued_at){
+        return false;
+    }
+    return true;
+}
+
+async function refreshToken(token){
+    let jwt = decJWT(token);
     let res = await fetch('https://api.fitbit.com/oauth2/token', {
         method: 'POST',
         headers: new Headers({
@@ -114,7 +127,7 @@ async function refreshToken(){
         body: new URLSearchParams({
             'grant_type': 'refresh_token',
             'client_id': '23Q7WF',
-            'refresh_token': JSON.parse(localStorage.getItem('fitbit-token')).refresh_token 
+            'refresh_token': jwt.refresh_token 
         }).toString()
     })
 
@@ -126,7 +139,11 @@ async function refreshToken(){
             errors: res.errors
         }
     } else {
+
         res.issued_at = Date.now();
+        const jwt = await encJWT(res);
+        localStorage.setItem('fitbit-token', jwt);
+
         return {
             success: true
         }
@@ -134,6 +151,7 @@ async function refreshToken(){
 }
 
 export {
+    decJWT,
     getAuthUrl,
     getToken,
     isValidToken,
