4048f6f729316dad13456fbdefb39e4262378981
AustinDecker <deckaj01@pfw.edu>
2025-03-26 14:10:56 -0400

    surrounding tests in a Meteor.isServer

--- imports/tests/fhirDataFunctions.tests.js
+++ imports/tests/fhirDataFunctions.tests.js
@@ -1,22 +1,24 @@
 import { Meteor } from 'meteor/meteor';
 import { assert } from 'chai';
 import sinon from 'sinon';
-
-import {
-    transformObservationInformation,
-    transformDiagonosticReportInformation,
-    getPatientObservation,
-    findByInfo,
-    getPatientHealthMetrics,
-    getRecentPatientLabs,
-    getPatientRecordByID
-} from '/imports/api/Fhir/Server/FhirUtils.js';
-
-describe('FHIR Data Functions', function () {
-    describe('transformObservationInformation', function () {
-        it('Should return null if the resource is null', function () {
-            const result = transformObservationInformation(null);
-            assert.isNull(result, 'Expected null when observation is null')
+if(Meteor.isServer) {
+    const {
+        transformObservationInformation,
+        transformDiagonosticReportInformation,
+        getPatientObservation,
+        findByInfo,
+        getPatientHealthMetrics,
+        getRecentPatientLabs,
+        getPatientRecordByID
+    } = await import('/imports/api/Fhir/Server/FhirUtils.js');
+    
+    describe('FHIR Data Functions', function () {
+        describe('transformObservationInformation', function () {
+            it('Should return null if the resource is null', function () {
+                const result = transformObservationInformation(null);
+                assert.isNull(result, 'Expected null when observation is null')
+            })
         })
     })
-})
+}
+
