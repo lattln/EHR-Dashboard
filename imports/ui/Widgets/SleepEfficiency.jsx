f6012e5d2931427f8f58769609847b5bd85a57cd
Lin Latt <lattln.tech@gmail.com>
2025-03-28 14:34:52 -0400

    Resolve conflicts
    + resolved merge conflicts.

--- imports/ui/Widgets/SleepEfficiency.jsx
+++ imports/ui/Widgets/SleepEfficiency.jsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
 import { getSleepEfficiency } from "../../api/FitBit/fitbit";
 
-function SleepEfficiencey({ fitBitLinked }) {
-    const [efficiency, setEfficiency] = useState(0);
+function SleepEfficiencey({ fitBitLinked }){
+	const [efficiency, setEfficiency] = useState(0);
+	const [sleepLogExists, setSleepLogExists] = useState(false);
 
-    useEffect(() => {
-        async function efficiency() {
-            let res = await getSleepEfficiency();
-            setEfficiency(res);
-        }
+	useEffect(() => {
+		async function efficiency(){
+			let res = await getSleepEfficiency();
 
-        efficiency();
-    }, [fitBitLinked])
+			if(res.efficiency != -1){
+				setSleepLogExists(true);
+				setEfficiency(res);
+			}
+		}
 
-    return (
-        <>
-            {
-                fitBitLinked ?
-                    <h1 className="text-9xl my-5 text-center">{efficiency}</h1>
-                    : "Link your FitBit account to access this widget"
-            }
-        </>
-    )
+		if(fitBitLinked){
+			efficiency();
+		}
+	}, [fitBitLinked])
+
+	return (
+		<>
+			<h2 className="text-lg font-bold">Sleep Efficiency</h2>
+			{
+				fitBitLinked ? 
+				sleepLogExists ? <h1 className="text-9xl my-5 text-center">{ efficiency }</h1> : <p>No Data Available</p> 
+				: "Link your FitBit account to access this widget"
+			}
+		</>
+	)
 };
 
 export default SleepEfficiencey;
