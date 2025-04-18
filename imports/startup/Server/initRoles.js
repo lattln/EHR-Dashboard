4031c8a85c2027d76ac510efce03e71ac2968c7b
AustinDecker <deckaj01@pfw.edu>
2025-04-18 11:45:55 -0400

    added accounts to be created for development.

--- imports/startup/Server/initRoles.js
+++ imports/startup/Server/initRoles.js
@@ -18,16 +18,66 @@ Meteor.startup(async () => {
         }
         
     }
+
+    async function createPatient(options={}){
+        const {email, password, firstName, lastName, dob, phoneNumber} = options;
+        await Meteor.callAsync("user.signup", {
+            email, 
+            password, 
+            firstName, 
+            lastName, 
+            dob, 
+            phoneNumber, 
+            role: UserRoles.PATIENT
+        })
+    }
     
     for (let role in UserRoles) {
         await Roles.createRoleAsync(UserRoles[role], {unlessExists: true})
     }
 
-    await createAdmin({
-        username: "ROOT",
-        password: "LIGMA"
-    });
+    try {
+        //root admin
+        await createAdmin({ 
+            username: "ROOT",
+            password: "LIGMA"
+        });
 
+        //patient accounts
+        await createPatient({
+            email: "test1@test.com",
+            password: "dummypatient01",
+            firstName: "Reggie481",
+            lastName: "Romaguera67",
+            dob: "2003-03-01",
+            phoneNumber: "555-657-6555",
+        });
+
+        await createPatient({
+            email: "test2@test.com",
+            password: "dummypatient02",
+            firstName: "Herman763",
+            lastName: "Schneider199",
+            dob: "1990-07-28",
+            phoneNumber: "555-949-2060",
+        });
+
+        await createPatient({
+            email: "test3@test.com",
+            password: "dummypatient03",
+            firstName: "Sean831",
+            lastName: "Luettgen772",
+            dob: "1988-02-28",
+            phoneNumber: "555-639-3016",
+        })
+
+        
+
+
+
+    } catch (error) {
+
+    }
 
 });
 
