46be0d116795f24878b590661c3b980f0143e192
Noah Nguyen <nohanguyenn@gmail.com>
2025-04-22 16:14:32 -0400

    Added settings.json support for FHIR Server

--- imports/api/Fhir/Server/fhirclient.js
+++ imports/api/Fhir/Server/fhirclient.js
@@ -1,11 +1,18 @@
  import Client from 'fhir-kit-client';
  import {Meteor} from "meteor/meteor";
+ import { logger } from '../../Logging/Server/logger-config.js';
 
 /**
  * object for accessing the specified fhir server.
  * provides a variety of methods to interact with the server, provided the 
  * server supports the actions.
  */
+
+const baseUrl = process.env.FHIR_SERVER_URL || Meteor.settings.private.FHIR_SERVER_URL
+
+
 export const fhirClient = new Client({
-    baseUrl: process.env.FHIR_SERVER_URL !== undefined ? process.env.FHIR_SERVER_URL : "http://localhost:8080/fhir"
+    baseUrl: baseUrl === undefined ? "http://localhost:8080/fhir" : baseUrl
 });
+
+logger.info(`Connected to FHIR Server ${baseUrl}.`)
